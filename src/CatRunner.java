39856e3d923419af3effd3efc8599c8928ff59e6
BT_3W2_28 <mateuszw7@nycstudents.net>
2022-09-29 12:12:09 -0400

    comm

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class CatRunner {
+public class CatRunner {
+    public static void main (String [] args){
+        Cat cat1 = new Cat ("Furry", 12, 10.3);
+        cat1.introduce();
+        cat1.printCatInfo();
+        cat1.playCat();
+
+        Cat cat2 = new Cat("Charles", 4, 4.5);
+        cat2.introduce();
+        cat2.printCatInfo();
+        cat2.playCat();
+    }
 }
